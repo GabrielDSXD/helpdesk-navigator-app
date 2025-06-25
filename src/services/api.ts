ab82c19ccb7668146b417b2b145947078e2e6d5b
Gabriel Dantas De Sousa <gabrieldantasousa1@gmail.com>
2025-06-25 10:02:52 -0300

    ops

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create a base axios instance
 const api = axios.create({
-  baseURL: 'http://192.168.0.3:3334:3333',
+  baseURL: 'http://192.168.0.3:3334',
   headers: {
     'Content-Type': 'application/json'
   }
